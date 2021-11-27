0cf845db4d869426ceae38a2b1a242d175745ae0
sasomiddinov1 <sasomiddinov1.isco6323@gmail.com>
2021-11-27 21:06:07 +0500

    first commit

---
+++ mainapp/tests.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+
+# Create your tests here.
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_cantect_page_status_code(self):
+        response = self.client.get('/cantact/')
+        self.assertEqual(response.status_code,200)
+
+    def test_blog_page_startus_code(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code,200)
